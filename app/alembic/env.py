64b37c80f164f35a19079e5c422184ddc6cbefde
jeryfan <1295448630@qq.com>
2025-06-18 16:40:58 +0000

    fix

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -17,7 +17,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from app.lib import db
+from app.models import db
 target_metadata = db.Base.metadata
 # target_metadata = None
 
